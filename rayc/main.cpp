9c9b3c57cb787ec43bad2cceff3a2bded6b34296
Jeremy Jacobson <jjacobson93@gmail.com>
2014-10-31 13:59:51 -0700

    Initial Commit

---
+++ rayc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rayc
+//
+//  Created by Jeremy Jacobson on 10/31/14.
+//  Copyright (c) 2014 Jeremy Jacobson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
